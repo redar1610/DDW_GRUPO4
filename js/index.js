57dada90f6a64b92e73e3ed51ef4587b8ba29f76
unknown <redar160>
2023-06-19 19:31:54 -0500

    actualizacion pagina semi terminada

--- js/index.js
+++ js/index.js
@@ -15,4 +15,15 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   });
+
+  const nav = document.querySelector("#nav");
+  const abrir = document.querySelector("#abrir");
+  const cerrar = document.querySelector("#cerrar");
   
+  abrir.addEventListener("click", () => {
+      nav.classList.add("visible");
+  })
+  
+  cerrar.addEventListener("click", () => {
+      nav.classList.remove("visible");
+  })
